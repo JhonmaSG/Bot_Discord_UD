99359fa1c7651f8cdd2954a036a6eec0f3d54cf2
JhonmaSG <jmserranog@udistrital.edu.co>
2024-09-09 12:24:35 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-// Archivo.index.js
-const Discord = require('dotenv').config();
-const {Client, Events} = require("discord.js");
+const {Client, Events} = require('discord.js');
+require('dotenv').config();
+const fs = require('fs');
 
-//Crear nuevo cliente de Discord
 const client = new Client({
-    intents: 53608447
+    intents: [53608447]
 });
 
-// Pimer Evento
 client.on(Events.ClientReady, async () => {
-    console.log(`Contectado como ${client.user.username}!`)
+    console.log(`Conectado como ${client.user.username}!`)
+})
+/*
+Client.on('ready', async (client) => {
+    console.log(`${client.user.tag} Online 🤖!`);
+    client.user.setActivity('Developed by JhonmaSG.', { type: 1 });
+});
+*/
+// Respuesta a mensajes
+client.on(Events.MessageCreate, async (message) => {
+    if (message.author.bot) return; // Si el autor del mensaje es un bot nos retiramos
+    if (!message.content.startsWith('!')) return;
+
+    const args = message.content.slice(1).split(' ')[0] //El contenido cuenta a partir del siguiente caracter
+
+    try {
+        const command = require(`./commands/${args}`);
+        command.run(message);
+    } catch (error) {
+        console.log(`${message.content} no es un comando valido.`);
+    }
 });
 
-// Respuestas a mesajes
-client.on(Events.MessageCreate, async (message) => {
-    if(message.author.bot) return; //Si el autor del mensaje es un bot, ns retiramos
-    if(!message.content.startsWith('-')) return; //Si el contenido del mensaje No comienza por - return
-    
-    const args = message.content.slice(1).split(' ')[0] //Filtra el primer caracter (-) del contenido
-
-    // Texto Command handler (Save informacion del command) en un archvo
-    try{
-        const command = require(`./commands/${args}`);
-        command.run(message)
-    } catch (error){
-        // Traffic
-        console.log(`Ha ocurrido un error al utilizar el commando -${args}`, error.message)
-    }
-})
-
-// Conexion cliente
 client.login(process.env.TOKEN_DC);
