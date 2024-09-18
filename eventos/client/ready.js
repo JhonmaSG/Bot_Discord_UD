85ddf9e5dec0ef05e9f715b012ff4542d9296fce
JhonmaSG <jmserranog@udistrital.edu.co>
2024-09-18 18:25:32 -0500

    fix header and commands

--- eventos/client/ready.js
+++ eventos/client/ready.js
@@ -3,7 +3,10 @@ const config = require('../../config/config.json');
 module.exports = client => {
     //Nos conectams a la DB
     mongoose.connect(
-        config.mongodb).then(() => {
+        config.mongodb, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }).then(() => {
         console.log(`⛅ Conectado a la Base de Datos de MONGODB`.blue);
     }).catch((err) => {
         console.log(`⛅ ERROR AL CONECTAR A LA BASE DE DATOS DE MONGODB`.red);
--- eventos/guild/messageCreate.js
+++ eventos/guild/messageCreate.js
@@ -1,24 +1,33 @@
 //Cada vez que se envie un mensaje, Creemos la BD y cargar el comando
 const config = require(`${process.cwd()}/config/config.json`);
-const serverSchema = require(`${process.cwd()}/modelos/servidor.js`);
-const {asegurar} = require(`${process.cwd()}/handlers/funciones.js`);
+const Discord = require('discord.js');
+const serverSchema = require(`${process.cwd()}/modelos/servidor.js`)
+const { asegurar_todo } = require(`${process.cwd()}/utils/funciones.js`)
+
+
 module.exports = async (client, message) => {
-    //si no hay servidor de mensaje, autor o es un bot, ignorar
-    if(!message.guild || !message.channel || message.author.bot) return;
-    let data = await asegurar(serverSchema, "guildID", message.guild.id, {
-        guildID: message.guild.id,
-        prefijo: config.prefix
-    });
-    //Si el contenido no empieza con el prefijo, retornar
-    if(!message.content.startsWith(data.prefijo)) return
+    if (!message.guild || !message.channel || message.author.bot) return;
+    await asegurar_todo(message.guild.id, message.author.id);
+    let data = await serverSchema.findOne({guildID: message.guild.id});
+
+    //si el bot es mencionado, devolvemos un mensaje de respuesta indicando el prefijo establecido en el servidor
+    if(message.content.includes(client.user.id)) return message.reply({
+        embeds: [
+            new Discord.EmbedBuilder()
+            .setTitle(`✅ **Para ver los comandos usa \`${data.prefijo}help\`!**`)
+            .setColor(client.color)
+        ]
+    })
+
+    if (!message.content.startsWith(data.prefijo)) return;
     const args = message.content.slice(data.prefijo.length).trim().split(" ");
     const cmd = args.shift()?.toLowerCase();
-    // intentar encontrar un comando del archivo javascript o un alias, ejecutar
-    const command = client.commands.get(cmd) || client.commands.find(c => c.aliases && c.aliases.include(cmd));
+    const command = client.commands.get(cmd) || client.commands.find(c => c.aliases && c.aliases.includes(cmd));
     if(command) {
-    // ejecutar el comando
-    command.run(client, message, args, data.prefijo)
+        // ejecutar el comando
+        command.run(client, message, args, data.prefijo)
     } else {
         return message.reply("❌ No he encontrado el comando que me has especificado!")
     }
+
 }
